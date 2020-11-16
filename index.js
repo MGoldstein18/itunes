9e3b00980505ecf07935f8478d80e5d61f012d2a
Mordi Goldstein <62645629+MGoldstein18@users.noreply.github.com>
2020-11-16 14:04:51 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,50 @@
+//import express, cors, node-fetch, helmet, body parser and declare an ENV variable for PORT
+const express = require("express");
+const cors = require('cors');
+const app = express();
+const PORT = process.env.PORT || 5000;
+const helmet = require("helmet");
+const bodyParser = require("body-parser");
+const fetch = require("node-fetch");
+const path = require('path');
+const publicPath = path.join(__dirname, '..', 'public');
+
+app.use(express.static(publicPath));
+
+//use cors
+app.use(cors());
+
+//use helmet
+app.use(helmet());
+
+//use body parser middle ware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(publicPath, 'index.html'))
+})
+
+//start the server on the port declared above
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});
+
+//create a variable called fetchData to make the API to the itunes API using params passed as arguments
+const fetchData = async (query, entity) => {
+  const apiCall = await fetch(
+    `https://itunes.apple.com/search?term=${query}&entity=${entity}`
+  );
+  const response = await apiCall.json();
+  //return the reponse from the API call
+  return response;
+};
+
+/*create a put request to handle the put request from the end. 
+Receive params from frontend and pass them to the fetchData variable which is called here*/
+app.put("/:query/:entity", (req, res) => {
+  const { query, entity } = req.params;
+  fetchData(query, entity)
+    .then((data) => res.json(data.results))
+    .catch((err) => console.log("Something broke"));
+});
---
+++ readme.md
@@ -0,0 +1,19 @@
+itunesSearch App
+-----------------------------------------
+
+What is it?
+The itunesSearch App is a full-stack app (react and express) which allows the user to search the itunes store. 
+The use can also select favourites from the results and and them to the favourites list. 
+
+How to install, run and test?
+The app is on Github and can be cloned to a local machine. 
+It the can be run by installing npm and using "npm start" - this command will start the backend and frontend because concurrently is installed. 
+Test can done by using "npm test" in the backend and frontend. A snapshot test is used in the frontend a mocha/chai test is used in the backend. 
+
+How to use the itunesSearch app?
+Enter the artist/song name or other term in the search bar. 
+Select a media type. 
+Click on the search button. 
+Enjoy the results!
+If you want to add a song/artist/album/other to your favourites clickon the add favourite button 
+
